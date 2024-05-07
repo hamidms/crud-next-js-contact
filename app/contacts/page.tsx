1eddfbd7c30d00b75c9a14cd86b63cbb6e10bdb4
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-07 22:15:09 +0700

    create table

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,8 +1,10 @@
+import ContactTable from "@/components/contact-table"
+
 const Contacts = () => {
   return (
     <div className="max-w-screen-md mx-auto mt-5">
         <div className="flex items-center justify-between gap-1 mb-5">
-            
+            <ContactTable/>
         </div>
     </div>
   )
