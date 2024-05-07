e1e9955650b4a9550fe19495fd62cb3105fdc82e
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-07 22:17:51 +0700

    create search window

---
+++ components/search.tsx
@@ -0,0 +1,9 @@
+const Search = () => {
+  return (
+    <div className="relative flex flex-1">
+        <input type="text" className="w-full border border-gray-200 py-2 pl-10 text-sm outline-2 rounded-sm" name="" id="" placeholder="search.."  />
+    </div>
+  )
+}
+
+export default Search
