6e7a8f3f17cb3c6ffb6d27911f26870fcb8d63ee
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-07 23:36:32 +0700

    create button components

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { IoAddSharp } from "react-icons/io5"
+import { IoAddSharp, IoPencil, IoTrashOutline } from "react-icons/io5"
 
 export const CreateButton = () => {
     return (
@@ -7,5 +7,21 @@ export const CreateButton = () => {
             <IoAddSharp size={20}/>
             Create
         </Link>
+    )
+}
+
+export const EditButton = () => {
+    return (
+        <Link href="/contacts/edit" className="rounded-sm border p-1 hover:bg-gray-100">
+            <IoPencil size={20}/>
+        </Link>
+    )
+}
+
+export const DeleteButton = () => {
+    return (
+        <button className="rounded-sm border p-1 hover:bg-gray-100">
+            <IoTrashOutline size={20}/>
+        </button>
     )
 }
