1a7930b858949072582e683204d21ea02f3389fe
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-08 00:13:40 +0700

    validate Create Component

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
+"use client"
+
+import { saveContact } from "@/lib/actions";
+import { useFormState } from "react-dom";
 
 const CreateForm = () => {
+    const [state, formAction] = useFormState(saveContact, null);
   return (
     <div>
-        <form action="">
+        <form action={formAction}>
             {/* Name */}
             <div className="mb-5">
                 <label htmlFor="name" className='block text-sm font-medium text-gray-900'>Full Name</label>
                 <input type="text" name='name' id='name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='Full Name..'/>
+                <div id="name-error" aria-live="polite" aria-atomic="true">
+                    <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
+                </div>
             </div>
 
             {/* Phone */}
             <div className="mb-5">
                 <label htmlFor="phone" className='block text-sm font-medium text-gray-900'>Phone Number</label>
                 <input type="text" name='phone' id='phone' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder='Phone Number..'/>
+                <div id="phone-error" aria-live="polite" aria-atomic="true">
+                    <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
+                </div>
             </div>
+
+            <div id="message-error" aria-live="polite" aria-atomic="true">
+                    <p className="mt-2 text-sm text-red-500">{state?.message}</p>
+                </div>
 
             <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center'>Save</button>
         </form>
---
+++ lib/actions.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const ContactSchema = z.object({
+    name: z.string().min(6),
+    phone: z.string().min(11)
+})
+
+export const saveContact = async (prevState: any, formData: FormData) => {
+    const validateFields = ContactSchema.safeParse(Object.fromEntries(formData.entries()));
+    
+    if (!validateFields.success) {
+        return {
+            Error: validateFields.error.flatten().fieldErrors
+        }
+    }
+
+    try {
+        await prisma.contact.create({
+            data: {
+                name: validateFields.data.name,
+                phone: validateFields.data.phone
+            }
+        })
+    } catch (error) {
+        return { message: "Failed to create contact"}
+    }
+
+    revalidatePath("/contacts");
+    redirect("/contacts")
+}
