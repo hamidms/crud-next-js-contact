55432896bd1a2d9c253f0514171c889f0a43e8b8
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-08 15:02:15 +0700

    create pagination

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,8 +1,10 @@
 import ContactTable from "@/components/contact-table"
 import Search from "@/components/search"
+import Pagination from "@/components/pagination"
 import { CreateButton } from "@/components/buttons"
+import { getContactPages } from "@/lib/data"
 
-const Contacts = ({
+const Contacts = async ({
     searchParams
   }:{
     searchParams?:{
@@ -14,6 +16,8 @@ const Contacts = ({
   const query = searchParams?.query || ""
   const currentPage = Number(searchParams?.page) || 1;
 
+  const totalPages = await getContactPages(query)
+
   return (
     <div className="max-w-screen-md mx-auto mt-5">
         <div className="flex items-center justify-between gap-1 mb-5">
@@ -21,6 +25,9 @@ const Contacts = ({
             <CreateButton />
         </div>
         <ContactTable query={query} currentPage={currentPage}/>
+        <div className="flex justify-center mt-4">
+          <Pagination totalPages={totalPages}/>
+        </div>
     </div>
   )
 }
